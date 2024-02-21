0460609393fe111ff98a5e32a93de5d4515bea8e
LucaEchle <luca.echle@codesystem.org>
2024-02-21 20:43:32 +0000

    added placeholder component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {TestContentComponent} from "./components/test-content/test-content.component";
 import {Error404Component} from "./components/error/error-404/error-404.component";
 import {keycloakGuard} from "./guard/keycloak.guard";
+import {PlaceholderComponent} from "./components/placeholder/placeholder.component";
 
 export const routes: Routes = [
   {path: '', pathMatch: 'full', component: TestContentComponent, canActivate: [keycloakGuard]},
-  {path: '**', pathMatch: 'full', component: Error404Component}
+  {path: 'agent', pathMatch: 'full', component: PlaceholderComponent, canActivate: [keycloakGuard]},
+  {path: 'package', pathMatch: 'full', component: PlaceholderComponent, canActivate: [keycloakGuard]},
+  {path: 'deployment', pathMatch: 'full', component: PlaceholderComponent, canActivate: [keycloakGuard]},
+  {path: 'setting', pathMatch: 'full', component: PlaceholderComponent, canActivate: [keycloakGuard]},
+  {path: '**', pathMatch: 'full', component: Error404Component, canActivate: [keycloakGuard]}
 ];
 
 @NgModule({
---
+++ src/app/components/placeholder/placeholder.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: true,
+    imports: [
+        MatButton,
+        MatCard,
+        MatCardContent,
+        MatCardTitle,
+        MatIcon,
+        RouterLink
+    ],
+  templateUrl: './placeholder.component.html',
+  styleUrl: './placeholder.component.scss'
+})
+export class PlaceholderComponent {
+
+}
