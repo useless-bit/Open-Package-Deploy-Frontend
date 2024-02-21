da47ec9c862d9928696ce045c7cf6b54f1826409
LucaEchle <luca.echle@codesystem.org>
2024-02-21 20:43:52 +0000

    code reformatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import {
   Router,
   RouteReuseStrategy,
   withDisabledInitialNavigation,
-  withHashLocation,
-  withRouterConfig
+  withHashLocation
 } from '@angular/router';
 
 import {routes} from './app.routes';
@@ -59,8 +58,8 @@ export const appConfig: ApplicationConfig = {
           variableService.loadVariables().then(() => {
             initializeKeycloak(keycloakService, variableService).then(() => {
               // Navigate back to the original URL while removing url parameters (from Keycloak)
-                router.navigateByUrl(currentUrl.split(separator)[0]).then(() => {
-                });
+              router.navigateByUrl(currentUrl.split(separator)[0]).then(() => {
+              });
               sharedService.emitInitFinished(true);
             });
           });
