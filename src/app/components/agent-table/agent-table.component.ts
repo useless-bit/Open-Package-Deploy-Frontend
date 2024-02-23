4b809da76ab206bb5efeb2127696d259732e7ac4
LucaEchle <luca.echle@codesystem.org>
2024-02-23 11:25:22 +0000

    added ability to show/hide columns

--- src/app/components/agent-table/agent-table.component.ts
+++ src/app/components/agent-table/agent-table.component.ts
@@ -14,10 +14,12 @@ import {
 } from "@angular/material/table";
 import {AgentEntity} from "../../service/api/entity/agentEntity";
 import {ApiService} from "../../service/api/api.service";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {LoadingFullscreenComponent} from "../loading-fullscreen/loading-fullscreen.component";
 import {MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
+import {MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from "@angular/material/expansion";
+import {MatChip, MatChipListbox, MatChipOption, MatChipSet} from "@angular/material/chips";
 
 @Component({
   selector: 'app-agent-table',
@@ -36,20 +38,31 @@ import {MatInput} from "@angular/material/input";
     NgIf,
     LoadingFullscreenComponent,
     MatFormField,
-    MatInput
+    MatInput,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatChipListbox,
+    MatChipOption,
+    NgForOf,
+    MatChip,
+    MatChipSet
   ],
   templateUrl: './agent-table.component.html',
   styleUrl: './agent-table.component.scss'
 })
 export class AgentTableComponent implements OnInit {
 
-  displayedColumns: string[] = ['uuid', 'name', 'registrationCompleted'];
   dataSource: MatTableDataSource<AgentEntity>;
   public dataLoaded: boolean = false;
+  public selectedColumns: String[] = ['name'];
+  public agentInstance: AgentEntity = {name: "", registrationCompleted: false, uuid: ""};
+  public agentKeys = Object.keys(this.agentInstance) as Array<keyof AgentEntity>;
 
   constructor(dataSource: MatTableDataSource<AgentEntity>,
               private apiService: ApiService) {
     this.dataSource = dataSource;
+    this.dataSource.filterPredicate = this.filterVisibleColumns.bind(this);
   }
 
   ngOnInit() {
@@ -58,6 +71,7 @@ export class AgentTableComponent implements OnInit {
         this.dataSource.data = response.agents;
       }
       this.dataLoaded = true;
+      this.dataSource.filter = "";
     });
   }
 
@@ -66,4 +80,24 @@ export class AgentTableComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  onAgentSelect(agent: String): void {
+    const index = this.selectedColumns.indexOf(agent);
+    if (index === -1) {
+      this.selectedColumns.push(agent);
+    } else {
+      this.selectedColumns.splice(index, 1);
+    }
+    this.dataSource.filter = this.dataSource.filter;
+  }
+
+  public isSelected(agent: string): boolean {
+    return this.selectedColumns.includes(agent);
+  }
+
+  public filterVisibleColumns(data: AgentEntity, filter: string): boolean {
+    return this.selectedColumns.some(column => {
+      const value = String(data[column as keyof AgentEntity]).toLowerCase();
+      return value.includes(filter);
+    });
+  }
 }
