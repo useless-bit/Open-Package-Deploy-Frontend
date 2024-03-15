60d50bd5b7ee768fbabb7e9b6b07bd77c34d9b0f
LucaEchle <luca.echle@codesystem.org>
2024-03-15 11:01:17 +0000

    added ability to show package status

--- src/app/components/home/home-overview/home-overview.component.ts
+++ src/app/components/home/home-overview/home-overview.component.ts
@@ -3,6 +3,7 @@ import {MatCard} from "@angular/material/card";
 import {PlaceholderComponent} from "../../placeholder/placeholder.component";
 import {HomeAgentOnlineComponent} from "../home-online-agents/home-agent-online.component";
 import {HomeAgentOutdatedComponent} from "../home-outdated-agents/home-agent-outdated.component";
+import {HomePackageProcessingComponent} from "../home-package-processing/home-package-processing.component";
 
 @Component({
   selector: 'app-home-overview',
@@ -12,6 +13,7 @@ import {HomeAgentOutdatedComponent} from "../home-outdated-agents/home-agent-out
     PlaceholderComponent,
     HomeAgentOnlineComponent,
     HomeAgentOutdatedComponent,
+    HomePackageProcessingComponent,
   ],
   templateUrl: './home-overview.component.html',
   styleUrl: './home-overview.component.scss'
