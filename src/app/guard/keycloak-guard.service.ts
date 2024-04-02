1a0e26d091f857c9b5012059ba3417b515654398
LucaEchle <luca.echle@codesystem.org>
2024-04-02 23:04:46 +0000

    code cleanup

--- src/app/guard/keycloak-guard.service.ts
+++ src/app/guard/keycloak-guard.service.ts
@@ -13,27 +13,27 @@ export class KeycloakGuard extends KeycloakAuthGuard {
     super(router, keycloak);
   }
 
-  public isAccessAllowed(route: ActivatedRouteSnapshot): Promise<boolean> {
-    return new Promise(async (resolve) => {
-      // Force the user to log in if currently unauthenticated.
-      if (!this.authenticated) {
-        const currentUrl = location.href;
+  public async isAccessAllowed(
+    route: ActivatedRouteSnapshot,
+  ) {
+    // Force the user to log in if currently unauthenticated.
+    if (!this.authenticated) {
+      const currentUrl = location.href;
 
-        await this.keycloak.login({
-          redirectUri: currentUrl
-        });
-      }
+      await this.keycloak.login({
+        redirectUri: currentUrl
+      });
+    }
 
-      // Get the roles required from the route.
-      const {roles: requiredRoles} = route.data;
+    // Get the roles required from the route.
+    const {roles: requiredRoles} = route.data;
 
-      // Allow the user to proceed if no additional roles are required to access the route.
-      if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
-        resolve(true);
-      }
+    // Allow the user to proceed if no additional roles are required to access the route.
+    if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
+      return true;
+    }
 
-      // Allow the user to proceed if all the required roles are present.
-      resolve(requiredRoles.every((role: any) => this.roles.includes(role)));
-    });
+    // Allow the user to proceed if all the required roles are present.
+    return requiredRoles.every((role) => this.roles.includes(role));
   }
 }
