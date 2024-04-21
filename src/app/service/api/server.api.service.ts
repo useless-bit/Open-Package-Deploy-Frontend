f5df0128c0a04aef33733b64576fcf6f93c90ebd
LucaEchle <luca.echle@codesystem.org>
2024-04-21 16:35:18 +0000

    code cleanup

--- src/app/service/api/server.api.service.ts
+++ src/app/service/api/server.api.service.ts
@@ -34,7 +34,7 @@ export class ServerApiService {
         },
         error: (error) => {
           this.apiService.errorHandling(error);
-          reject(error);
+          reject(new Error(error));
         }
       });
     });
